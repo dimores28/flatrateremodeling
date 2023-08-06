786c49382956fa5ce7cac49f6f75f30543e3ce02
Dmytro <dmytroivanovichn@gmail.com>
2023-08-06 18:08:23 +0300

    fix style form

--- header.php
+++ header.php
@@ -39,6 +39,9 @@
             }
          }
    </style>
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&family=Roboto&display=swap" rel="stylesheet">
    
    <?php wp_head(); ?>
 </head>
