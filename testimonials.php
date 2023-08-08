5a246d8615c253bf1201eb29e0f8c0cc83c45e11
Dmytro <dimores95@gmail.com>
2023-08-08 15:58:13 +0300

    added blog template

--- testimonials.php
+++ testimonials.php
@@ -5,5 +5,29 @@ Template Name: Testimonials
 ?>
 
 <?php get_header();?>
+<div class="side-column-section">
+    <div class="side-column-section__content">
 
+    </div>
+    <aside class="side-column-section__aside">
+    <div class="aside-form">
+        <h2 class="aside-form__title">
+        Schedule A Free Consultation
+        </h2>
+        <div class="feedback">
+        <form class="feedback__form">
+            <input class="form-control" name="yourName" type="text" placeholder="Name" aria-label="your name" />
+            <input class="form-control" name="yourEmail" type="email" placeholder="Email" aria-label="your email" />
+            <input class="form-control" name="yourPhone" type="text" placeholder="Phone" aria-label="your phone" />
+            <textarea class="form-control" name="yourMessage" placeholder="Message" aria-label="your message"></textarea>
+            <button type="submit" class="aside-form__btn">
+            <span>Send</span>
+            <i aria-hidden="true" class="fas fa-long-arrow-alt-right"></i>
+            </button>
+        </form>
+        </div>
+    </div>
+    </aside>
+
+</div>
 <?php get_footer();?>
