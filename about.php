40bedaded17d5730884049035039ab7970604e14
maksym-krasovskyi <maxkrasovskyi10@gmail.com>
2023-08-08 22:20:45 +0300

    add about

--- about.php
+++ about.php
@@ -6,4 +6,53 @@ Template Name: About
 
 <?php get_header();?>
 
+<section class="about" id="about">
+          <div class="about__header">
+            <img
+              class="about__bg"
+              src="@img/bg-subheader-about.jpg"
+              alt="about__bg"
+            />
+            <div class="about__heading">
+              <h2>ABOUT US</h2>
+            </div>
+          </div>
+          <div class="about__body">
+            <div class="about__img">
+              <img src=" <?php bloginfo('template_url'); ?>/assets/img/Certificate-2021.webp" width="1348" height="520" alt="Certificate" />
+            </div>
+            <div class="about__content">
+              <h3 class="about__title">About us</h3>
+              <hr class="devider" color="#bb945b" size="5px" />
+              <p class="about__text">
+                Flat Rate Remodeling is a licensed, bonded and insured general
+                contracting company who guarantees to exceed your construction
+                and remodeling expectations. We work to create your dream home
+                step by step. We are a family owned business, meaning we
+                understand our clients and know the importance of maintaining a
+                home. we are the only remodeling company in Southern California
+                that offers "flat rate" pricing for all of our products and
+                services. You will never have to worry about fluctuating prices
+                during your project. We work with premium architects and
+                designers to provide their expertise and skills to any room in
+                your home.
+              </p>
+            </div>
+            <div class="why-us__img">
+              <img src="<?php bloginfo('template_url'); ?>/assets/img/why_us.jpg" width="1348" height="520" alt="why_us" />
+            </div>
+            <div class="why-us__content">
+              <!-- <h3 class="why-us__title">Why choose us?</h3> -->
+              <hr class="devider" color="#bb945b" size="5px" />
+              <p class="why-us__text">
+                We are also an environmentally friendly company with our energy
+                efficient products. We offer a variety of insulated windows and
+                doors, natural tankless water heaterS, solar panels, wireless
+                generators and more. we always provide each and every customer
+                first-rate work at a flat rate price!
+              </p>
+            </div>
+          </div>
+        </section>
+
 <?php get_footer();?>
