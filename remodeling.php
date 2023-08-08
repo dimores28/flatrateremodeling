5d8a4c800c974811a9158a244460ec68bfe54fe4
Dmytro <dmytroivanovichn@gmail.com>
2023-08-09 00:54:41 +0300

    fix testimonials

--- remodeling.php
+++ remodeling.php
@@ -8,16 +8,16 @@ Template Name: Remodeling
    <?php  the_content(); ?>
    
    <main class="main-services">
-   <?php echo do_shortcode('[logoshowcase]'); ?>
-
     <h2 class="service-heding">Our Customers<span>Love Our Work!</span></h2>
-    <h3 class="reviews-title">Customers Testimonials</h3>
+    <h3 class="reviews-title" style="font-size: 17px;">Customers Testimonials</h3>
 
     <?php echo do_shortcode('[trustindex no-registration=google]'); ?>
+
+    <?php echo do_shortcode('[logoshowcase arrows="false" autoplay="false" limit="4" extra_class="logo-slider"]'); ?>
    </main>
    
 
-   <section class="get-started">
+   <section class="get-started" style="margin-top: 30px;">
    <div class="get-started__body">
       <p class="get-started__text">Lets Get Started on Your Next Project!</p>
       <a id="btn_open" 
