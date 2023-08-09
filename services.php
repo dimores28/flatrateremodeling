1fdef61e486bd4ae5cd1c9e0136807eae9f58990
dimores <dimores95@gmail.com>
2023-08-10 00:22:15 +0300

    fix services

--- services.php
+++ services.php
@@ -9,7 +9,7 @@ Template Name: Services
    <main class="main-services">
       <?php  the_content(); ?>
 
-    <h2 class="service-heding" style="margin-top: 30px;">Our Customers<span>Love Our Work!</span></h2>
+    <h2 class="service-heding" style="margin-top: 40px;">Our Customers <span>Love Our Work!</span></h2>
     <h3 class="reviews-title" style="font-size: 17px;">Customers Testimonials</h3>
 
     <?php echo do_shortcode('[trustindex no-registration=google]'); ?>
@@ -18,7 +18,7 @@ Template Name: Services
    </main>
    
 
-   <section class="get-started" style="margin-top: 30px;">
+   <section class="get-started" style="margin-top: 20px;">
    <div class="get-started__body">
       <p class="get-started__text">Lets Get Started on Your Next Project!</p>
       <a id="btn_open" 
