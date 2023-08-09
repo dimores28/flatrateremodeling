e43cb1ab42371f275814b4638d2f1aec1e276715
Dmytro <dimores95@gmail.com>
2023-08-09 12:03:43 +0300

    chenge size logo

--- gallery.php
+++ gallery.php
@@ -6,4 +6,16 @@ Template Name: Gallery
 
 <?php get_header();?>
 
+<section class="get-started" style="margin-top: 30px;">
+   <div class="get-started__body">
+      <p class="get-started__text">Lets Get Started on Your Next Project!</p>
+      <a id="btn_open" 
+         class="get-started__button popup-link" 
+         style="text-decoration: none;"
+         href="#popup_1"
+      >
+         Start here
+      </a>
+   </div>
+</section>
 <?php get_footer();?>
