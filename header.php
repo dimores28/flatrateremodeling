d2504e2b38b8e76d5027ecefb4cb701d0e925357
Dmytro <dimores95@gmail.com>
2023-08-10 13:30:46 +0300

    add favicon

--- header.php
+++ header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-   <meta charset="UTF-8">
+   <meta charset="<?php bloginfo('charset'); ?>" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>FLATRATE</title>
+
+   <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+   <link rel="mask-icon" href="assets/img/safari-pinned-tab.svg" color="#5bbad5">
+   <meta name="msapplication-TileColor" content="#da532c">
+   <meta name="theme-color" content="#ffffff">
 
    <style>
         .video-section {
